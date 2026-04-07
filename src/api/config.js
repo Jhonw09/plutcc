c9e0e631f10acd7067c57568de0d6390cfa918b2
jao-prado <rm95221@estudante.fieb.edu.br>
2026-04-07 17:44:25 -0300

    finalizando o crud

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
-export const API_BASE = 'http://localhost:8080/api/v1'
+export const API_BASE = 'https://studyconnect-8bfr.onrender.com/api/v1'
 
 export const ENDPOINTS = {
   login:      `${API_BASE}/auth/login`,
