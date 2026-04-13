27f85c1a6d02577558cd2c77b00b38cdf763ad0d
jao-prado <rm95221@estudante.fieb.edu.br>
2026-04-13 00:31:06 -0300

    feat: add blog

---
+++ src/pages/BlogPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './TrilhaPage.module.css'
+
+export default function BlogPage() {
+  return (
+    <main style={{ padding: '3rem', textAlign: 'center' }}>
+      <h1>Sobre</h1>
+      <p>
+        Esta página ainda está em construção. Volte em breve para mais informações.
+      </p>
+    </main>
+  )
+}
